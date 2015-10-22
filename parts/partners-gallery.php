c3b376f32e6bb04bf9b5c324ba4e5873be182a53
fowlercraig <craig@wearegiants.com>
2015-10-22 15:23:08 -0700

    added slider to stupid logos

--- parts/partners-gallery.php
+++ parts/partners-gallery.php
@@ -1,14 +1,26 @@
+<div class="carousel">
+
 <?php 
 
 $images = get_field('gallery');
-if( $images ): ?>
+if( $images ): 
+$i = 1;
+echo '<div class="slide">';
+?>
 
 <?php foreach( $images as $image ): ?>
+<div class="partner">
+  <a href="#">
+    <?php echo $counter; ?>
+    <img class="img-responsive" src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+  </a>
+</div>
 
-<a href="#">
-<img class="img-responsive" src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
-</a>
-<hr class="compact invisible">
+<?php if($i % 6 == 0) {echo '</div><div class="slide">';} ?>
 
+<?php $i++; // add one per row ?>  
 <?php endforeach; ?>
 <?php endif; ?>
+<?php echo '</div>'; ?>
+
+</div>
