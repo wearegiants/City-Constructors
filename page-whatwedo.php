bb14f98c1142118903d2f6b6a596ba2b5bd350b0
fowlercraig <craig@wearegiants.com>
2015-10-22 16:54:20 -0700

    junk

--- page-whatwedo.php
+++ page-whatwedo.php
@@ -8,7 +8,7 @@
       <div class="page-content bg-color--gray wwd">
         <div class="fs-row">
           <hr class="invisible fs-cell fs-lg-full fs-md-full fs-sm-full">
-          <div class="fs-cell fs-lg-10 fs-md-6 fs-sm-3 fs-centered">
+          <div class="fs-cell fs-lg-11 fs-md-6 fs-sm-3 fs-centered">
             <div class="fs-row">
               <div class="wwd fs-cell fs-lg-8 fs-md-6 fs-sm-3">
                 <?php 
@@ -16,15 +16,16 @@
                   while ( have_rows('what_we_do') ) : the_row();
                 ?>
                 <div class="wwd-item fs-row">
-                  <div class="fs-cell fs-lg-3 fs-md-1 fs-sm-hide"><img src="/assets/img/1444351047_18.Pictures-Day.svg" class="img-responsive" /></div>
-                  <div class="fs-cell fs-lg-8 fs-lg-push-1 fs-md-5 fs-sm-3"><?php the_sub_field('description'); ?></div>
-                  <hr class="fs-cell fs-all-full invisible"/>
+                  <div class="fs-cell fs-lg-11 fs-lg-push-1 fs-md-5 fs-sm-3"><?php the_sub_field('description'); ?></div>
+                  <hr class="fs-cell fs-all-full divider"/>
+                  <br>
                 </div>
                 <?php endwhile; endif; ?>
               </div>
 
               <div class="fs-cell fs-lg-3 fs-md-6 fs-sm-3 fs-right">
                 <h3 class="section-title"><span>Selected Clients</span></h3>
+                <br>
                 <?php include locate_template('parts/partners-gallery.php' );?>
               </div>
             </div>
--- parts/partners-gallery.php
+++ parts/partners-gallery.php
@@ -10,10 +10,9 @@ echo '<div class="slide">';
 
 <?php foreach( $images as $image ): ?>
 <div class="partner fs-cell fs-xl-full fs-lg-full fs-md-fourth fs-sm-third fs-contained">
-  <a href="#">
     <?php echo $counter; ?>
     <img class="img-responsive" src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
-  </a>
+    <br>
 </div>
 
 <?php 
