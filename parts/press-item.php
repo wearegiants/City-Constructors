6173687df89e647121a1f790ffd60a08d067bf05
fowlercraig <craig@wearegiants.com>
2015-10-22 17:33:32 -0700

    things

--- parts/press-item.php
+++ parts/press-item.php
@@ -1,5 +1,22 @@
 <div class="press-item">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail('gallery-md', array('class' => 'img-responsive')); } ?>
 <h4 class="press-title"><?php the_title(); ?></h4>
-<a href="<?php the_permalink(); ?>" class="btn btn-link btn-first btn-icon ss-gizmo ss-plus">Learn More</a>
+<?php 
+
+  $pressLink = get_the_permalink();
+
+  if (get_field('press_url')) {
+
+    $pressLink = get_field('press_url');
+
+  }
+
+  if (get_field('press_download')) {
+
+    $pressLink = get_field('press_download');
+
+  }
+
+?>
+<a href="<?php echo $pressLink; ?>" class="btn btn-link btn-first btn-icon ss-gizmo ss-plus">Learn More</a>
 </div>
