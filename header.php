cdacfd248d02cbfac623cbf127e0fb21cba8b9d5
Craig Fowler <craig@wearegiants.com>
2015-10-27 07:41:22 -0700

    updated the header title

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="utf-8">
-<title><?php bloginfo( 'sitename' ) ?></title>
+<title><?php wp_title( ' | ', true, 'right' ); ?><?php bloginfo( 'sitename' ); ?></title>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="description" content="<?php bloginfo( 'description' ) ?>">
 <link rel="shortcut icon" href="/assets/img/favicon.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
