d7e4f0b10b98c7ce95fcebb89eedb736f3d9be5c
fowlercraig <craig@wearegiants.com>
2015-10-27 13:32:32 -0700

    updated the job link

--- parts/jobs.php
+++ parts/jobs.php
@@ -1,7 +1,7 @@
 <?php if( have_rows('positions') ): while ( have_rows('positions') ) : the_row(); ?>
 
 <div class="fs-cell fs-lg-full fs-md-6 fs-sm-3">
-<a class="btn btn-link btn-icon btn-fullwidth btn-first ss-gizmo ss-plus" target="blank" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('job'); ?></a>
+<a class="btn btn-link btn-icon btn-fullwidth btn-first ss-gizmo ss-plus" target="blank" href="<?php the_sub_field('job_url'); ?>"><?php the_sub_field('job'); ?></a>
 </div>
 
 <?php endwhile; endif;?>
